78193622ac85c89948f8dcd9eb7b6be34bfb06cd
danielthornsjo <danielthornsjo@live.se>
2026-01-14 21:07:46 +0100

    Portfolio section with karstorp project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
+const accordionHeaders = document.querySelectorAll(".accordion-header");
 
 burger.addEventListener("click", () => {
     nav.classList.toggle('show');
     burger.classList.toggle('active')
+});
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const content = header.nextElementSibling;
+
+        accordionHeaders.forEach(otherHeader => {
+            if (otherHeader !== header) {
+                otherHeader.classList.remove('active');
+                otherHeader.nextElementSibling.style.maxHeight = null;
+                otherHeader.nextElementSibling.classList.remove('active');
+            }
+        });
+
+        header.classList.toggle('active');
+        content.classList.toggle('active');
+
+        if (content.classList.contains('active')) {
+            content.style.maxHeight = content.scrollHeight + "px";
+        } else {
+            content.style.maxHeight = null;
+        }
+    });
 });
